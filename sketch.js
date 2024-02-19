41ddacafe4bd27695ad5cc035ec8581b5a43c40c
Brigham Pettit <pettit@DESKTOP-MG3DLQJ>
2024-02-19 12:19:10 -0600

    initial commit idk

---
+++ sketch.js
@@ -0,0 +1,31 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  
+}
+
+// fake Zoom
+
+// interaction #1
+  // open the app (click)
+
+// #2
+  // start a meeting (click)
+
+// interaction #2
+  // slider to change audio levels (slider)
+
+// #3
+  // upload a profile picture (upload)
+
+// #4 
+  // change your name on Zoom call (typing)
+
+// #5 
+  // toggle between name, profile, camera (?)
+
+// #6
+  // using the camera 
